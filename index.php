4f64381874a3f7e3dcbd9d59d381747ef544ddf2
PabloFC <pablofc_68@hotmail.com>
2025-04-15 00:39:09 +0200

    index.php modified

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-
-<?php include(get_template_directory() . '/includes/seccion-1.php'); ?>
-<?php include(get_template_directory() . '/includes/seccion-2.php'); ?>
-<?php include(get_template_directory() . '/includes/seccion-3.php'); ?>
-
+<?php if (is_front_page()): ?>
+    <?php include('includes/seccion-1.php'); ?>
+    <?php include('includes/seccion-2.php'); ?>
+    <?php include('includes/seccion-3.php'); ?>
+<?php endif; ?>
 <?php get_footer(); ?>
