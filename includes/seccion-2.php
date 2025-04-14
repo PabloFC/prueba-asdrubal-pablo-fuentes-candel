51d0b4dd140c34ba9f414bf06583e014e47fd743
PabloFC <pablofc_68@hotmail.com>
2025-04-14 19:37:31 +0200

    cleaning code in section 2

--- includes/seccion-2.php
+++ includes/seccion-2.php
@@ -1,50 +1,53 @@
+<section class="seccion-normal mx-auto">
+    <div>
+        <h2 class="text-center pb-1">El Equipo</h2>
+        <h4 class="text-center pb-1">Los que van a tratar tu empresa con la importancia que se merece</h4>
+    </div>
+    <div>
+        <hr class="custom-hr">
+    </div>
 
-<section style="max-width:1400px;" class="seccion-normal mx-auto">
-        <div>
-            <h2 class="text-center pb-1">El Equipo</h2>
-            <h4 class="text-center pb-1">Los que van a tratar tu empresa con la importancia que se merece</h4>
-        </div>
-        <div >
-          <hr class="custom-hr">
-        </div>
+    <div class="row row-cols-1 row-cols-md-2 g-4 p-5">
+        <?php 
+        $equipo = [
+            [
+                'nombre' => 'Carlos Sánchez',
+                'descripcion' => 'El generador de soluciones',
+                'imagen' => 'carlos-sanchez.jpg',
+                'alt' => 'Carlos Sánchez'
+            ],
+            [
+                'nombre' => 'Roberto Martínez González',
+                'descripcion' => 'El estratega',
+                'imagen' => 'roberto-seo.jpg',
+                'alt' => 'Roberto Martínez'
+            ],
+            [
+                'nombre' => 'Rosario Aznar',
+                'descripcion' => 'La experta en Organización',
+                'imagen' => 'rosario.jpg',
+                'alt' => 'Rosario Aznar'
+            ],
+            [
+                'nombre' => 'Sandra Santos',
+                'descripcion' => 'La fanática del código',
+                'imagen' => 'sandra-santos.jpg',
+                'alt' => 'Sandra Santos'
+            ]
+        ];
 
-        <div class="row row-cols-1 row-cols-md-2 g-4 p-5">
-            
+        foreach ($equipo as $miembro): ?>
             <div class="col">
-            <div class="card h-100">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/carlos-sanchez.jpg" class="card-img-top" alt="Carlos Sánchez">
-                <div class="card-body">
-                <h5 class="card-title text-center">Carlos Sánchez</h5>
-                <p class="card-text text-center">El generador de soluciones</p>
+                <div class="card h-100">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/<?php echo $miembro['imagen']; ?>" 
+                         class="card-img-top" 
+                         alt="<?php echo $miembro['alt']; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><?php echo $miembro['nombre']; ?></h5>
+                        <p class="card-text text-center"><?php echo $miembro['descripcion']; ?></p>
+                    </div>
                 </div>
             </div>
-            </div>
-            <div class="col">
-            <div class="card h-100">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/roberto-seo.jpg" class="card-img-top" alt="Roberto Martínez">
-                <div class="card-body">
-                <h5 class="card-title text-center">Roberto Martínez Gonazlez</h5>
-                <p class="card-text text-center">El estratega</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card h-100">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/rosario.jpg" class="card-img-top" alt="Rosario Aznar">
-                <div class="card-body">
-                <h5 class="card-title text-center">Rosario Aznar</h5>
-                <p class="card-text text-center">La experta en Organización</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card h-100">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/sandra-santos.jpg" class="card-img-top" alt="Rosario Aznar">
-                <div class="card-body">
-                <h5 class="card-title text-center">Sandra Santos</h5>
-                <p class="card-text text-center">La fanática del código</p>
-                </div>
-            </div>
-            </div>
-        </div>
-    </section>
+        <?php endforeach; ?>
+    </div>
+</section>
